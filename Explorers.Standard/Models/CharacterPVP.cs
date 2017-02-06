972a5b7a5972feed02f0eba34c08aaed3d13a371
Feroks <mp@best.lv>
2017-02-06 22:28:11 +0200

    Added .Net standard project and copied the code.

---
+++ Explorers.Standard/Models/CharacterPVP.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Runtime.Serialization;
+
+namespace WowDotNetAPI.Models
+{
+    [DataContract]
+    public class CharacterPvP
+    {
+        [DataMember(Name = "brackets")]
+        public CharacterPvPBrackets Brackets { get; set; }
+    }
+}
---
+++ Explorers.Standard/Models/Raid.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace WowDotNetAPI.Models
+{
+    [DataContract]
+    public class Raid
+	{
+        [DataMember(Name="name")]
+		public string Name { get; set; }
+
+        [DataMember(Name = "lfr")]
+        public int LFR { get; set; }
+
+        [DataMember(Name = "normal")]
+        public int Normal { get; set; }
+
+        [DataMember(Name = "heroic")]
+        public int Heroic { get; set; }
+
+        [DataMember(Name = "mythic")]
+        public int Mythic { get; set; }
+
+        [DataMember(Name = "id")]
+        public int Id { get; set; }
+
+        [DataMember(Name = "bosses")]
+        public IEnumerable<RaidBoss> Bosses { get; set; }
+	}
+}
